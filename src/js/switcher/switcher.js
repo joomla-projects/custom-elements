0e0ffb939f00271ac67fca2d2d4bbc3ae36c758d
C-Lodder <lodder15@hotmail.co.uk>
2017-08-21 10:26:20 +0100

    Add switcher web component

---
+++ src/js/switcher/switcher.js
@@ -0,0 +1,71 @@
+(function () {
+	const css = `{{stylesheet}}`;
+	if (!document.getElementById('joomla-switcher-stylesheet')) {
+		const style = document.createElement('style');
+		style.id = 'joomla-switcher-stylesheet';
+		style.innerHTML = css;
+		document.head.appendChild(style);
+	}
+})();
+
+class SwitcherElement extends HTMLElement {
+	/* Lifecycle, element created */
+	constructor() {
+		super();
+	}
+
+	/* Lifecycle, element appended to the DOM */
+	connectedCallback() {
+		// Add the initial active class
+		const switcher = this.querySelectorAll('input'),
+			  next     = switcher[1].parentNode.nextElementSibling;
+
+		if (switcher[1].checked) {
+			switcher[1].parentNode.classList.add('active');
+			next.querySelector('.switcher-label-' + switcher[1].value).classList.add('active');
+		}
+		else
+		{
+			next.querySelector('.switcher-label-' + switcher[0].value).classList.add('active');
+		}
+
+		// Add the active class on click
+		this.addEventListener('click', function(event) {
+			let el     = event.target,
+				parent = el.parentNode,
+				spans  = parent.nextElementSibling.querySelectorAll('span');
+
+			for (let i = 0; i < spans.length; i++) {
+				spans[i].classList.remove('active');
+			}
+
+			if (!el.classList.contains('active')) {
+				parent.classList.add('active');
+				this.dispatchCustomEvent('joomla.switcher.on');
+			}
+			else {
+				parent.classList.remove('active');
+				this.dispatchCustomEvent('joomla.switcher.off');
+			}
+
+			parent.nextElementSibling.querySelector('.switcher-label-' + el.value).classList.add('active');
+		});
+	}
+
+	/* Lifecycle, element removed from the DOM */
+	disconnectedCallback() {
+		this.removeEventListener('joomla.switcher.toggle', this);
+		this.removeEventListener('joomla.switcher.on', this);
+		this.removeEventListener('joomla.switcher.off', this);
+		this.removeEventListener('click', this);
+	}
+
+	/* Method to dispatch events */
+	dispatchCustomEvent(eventName) {
+		let OriginalCustomEvent = new CustomEvent(eventName, {bubbles: true, cancelable: true});
+		OriginalCustomEvent.relatedTarget = this;
+		this.dispatchEvent(OriginalCustomEvent);
+		this.removeEventListener(eventName, this);
+	}
+}
+customElements.define('joomla-switcher', SwitcherElement);
