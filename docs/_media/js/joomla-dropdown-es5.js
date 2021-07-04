9dc607c2245a93f937fa7600f354fa2e016c161a
dgrammatiko <d.grammatiko@gmail.com>
2021-07-04 16:37:59 +0200

    Only Tools Update

--- docs/_media/js/joomla-dropdown-es5.js
+++ docs/_media/js/joomla-dropdown-es5.js
@@ -1,295 +1,142 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-"use strict";
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
-function _typeof(obj) {
-  "@babel/helpers - typeof";
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-  if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") {
-    _typeof = function _typeof(obj) {
-      return typeof obj;
-    };
-  } else {
-    _typeof = function _typeof(obj) {
-      return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
-    };
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _wrapNativeSuper(Class) { var _cache = typeof Map === "function" ? new Map() : undefined; _wrapNativeSuper = function _wrapNativeSuper(Class) { if (Class === null || !_isNativeFunction(Class)) return Class; if (typeof Class !== "function") { throw new TypeError("Super expression must either be null or a function"); } if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() { return _construct(Class, arguments, _getPrototypeOf(this).constructor); } Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writable: true, configurable: true } }); return _setPrototypeOf(Wrapper, Class); }; return _wrapNativeSuper(Class); }
+
+function _construct(Parent, args, Class) { if (_isNativeReflectConstruct()) { _construct = Reflect.construct; } else { _construct = function _construct(Parent, args, Class) { var a = [null]; a.push.apply(a, args); var Constructor = Function.bind.apply(Parent, a); var instance = new Constructor(); if (Class) _setPrototypeOf(instance, Class.prototype); return instance; }; } return _construct.apply(null, arguments); }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
+
+function _isNativeFunction(fn) { return Function.toString.call(fn).indexOf("[native code]") !== -1; }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var JoomlaDropdownElement = /*#__PURE__*/function (_HTMLElement) {
+  _inherits(JoomlaDropdownElement, _HTMLElement);
+
+  var _super = _createSuper(JoomlaDropdownElement);
+
+  function JoomlaDropdownElement() {
+    _classCallCheck(this, JoomlaDropdownElement);
+
+    return _super.apply(this, arguments);
   }
 
-  return _typeof(obj);
-}
-
-function _classCallCheck(instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-}
-
-function _defineProperties(target, props) {
-  for (var i = 0; i < props.length; i++) {
-    var descriptor = props[i];
-    descriptor.enumerable = descriptor.enumerable || false;
-    descriptor.configurable = true;
-    if ("value" in descriptor) descriptor.writable = true;
-    Object.defineProperty(target, descriptor.key, descriptor);
-  }
-}
-
-function _createClass(Constructor, protoProps, staticProps) {
-  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
-  if (staticProps) _defineProperties(Constructor, staticProps);
-  return Constructor;
-}
-
-function _inherits(subClass, superClass) {
-  if (typeof superClass !== "function" && superClass !== null) {
-    throw new TypeError("Super expression must either be null or a function");
-  }
-
-  subClass.prototype = Object.create(superClass && superClass.prototype, {
-    constructor: {
-      value: subClass,
-      writable: true,
-      configurable: true
+  _createClass(JoomlaDropdownElement, [{
+    key: "for",
+    get: function get() {
+      return this.getAttribute('for');
+    },
+    set: function set(value) {
+      return this.setAttribute('for', value);
     }
-  });
-  if (superClass) _setPrototypeOf(subClass, superClass);
-}
+  }, {
+    key: "connectedCallback",
+    value: function connectedCallback() {
+      var _this = this;
 
-function _createSuper(Derived) {
-  var hasNativeReflectConstruct = _isNativeReflectConstruct();
+      this.setAttribute('aria-labelledby', this["for"].substring(1));
+      var button = document.querySelector(this["for"]);
+      var innerLinks = this.querySelectorAll('a');
 
-  return function _createSuperInternal() {
-    var Super = _getPrototypeOf(Derived),
-        result;
-
-    if (hasNativeReflectConstruct) {
-      var NewTarget = _getPrototypeOf(this).constructor;
-
-      result = Reflect.construct(Super, arguments, NewTarget);
-    } else {
-      result = Super.apply(this, arguments);
-    }
-
-    return _possibleConstructorReturn(this, result);
-  };
-}
-
-function _possibleConstructorReturn(self, call) {
-  if (call && (_typeof(call) === "object" || typeof call === "function")) {
-    return call;
-  }
-
-  return _assertThisInitialized(self);
-}
-
-function _assertThisInitialized(self) {
-  if (self === void 0) {
-    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
-  }
-
-  return self;
-}
-
-function _wrapNativeSuper(Class) {
-  var _cache = typeof Map === "function" ? new Map() : undefined;
-
-  _wrapNativeSuper = function _wrapNativeSuper(Class) {
-    if (Class === null || !_isNativeFunction(Class)) return Class;
-
-    if (typeof Class !== "function") {
-      throw new TypeError("Super expression must either be null or a function");
-    }
-
-    if (typeof _cache !== "undefined") {
-      if (_cache.has(Class)) return _cache.get(Class);
-
-      _cache.set(Class, Wrapper);
-    }
-
-    function Wrapper() {
-      return _construct(Class, arguments, _getPrototypeOf(this).constructor);
-    }
-
-    Wrapper.prototype = Object.create(Class.prototype, {
-      constructor: {
-        value: Wrapper,
-        enumerable: false,
-        writable: true,
-        configurable: true
-      }
-    });
-    return _setPrototypeOf(Wrapper, Class);
-  };
-
-  return _wrapNativeSuper(Class);
-}
-
-function _construct(Parent, args, Class) {
-  if (_isNativeReflectConstruct()) {
-    _construct = Reflect.construct;
-  } else {
-    _construct = function _construct(Parent, args, Class) {
-      var a = [null];
-      a.push.apply(a, args);
-      var Constructor = Function.bind.apply(Parent, a);
-      var instance = new Constructor();
-      if (Class) _setPrototypeOf(instance, Class.prototype);
-      return instance;
-    };
-  }
-
-  return _construct.apply(null, arguments);
-}
-
-function _isNativeReflectConstruct() {
-  if (typeof Reflect === "undefined" || !Reflect.construct) return false;
-  if (Reflect.construct.sham) return false;
-  if (typeof Proxy === "function") return true;
-
-  try {
-    Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {}));
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
-
-function _isNativeFunction(fn) {
-  return Function.toString.call(fn).indexOf("[native code]") !== -1;
-}
-
-function _setPrototypeOf(o, p) {
-  _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
-    o.__proto__ = p;
-    return o;
-  };
-
-  return _setPrototypeOf(o, p);
-}
-
-function _getPrototypeOf(o) {
-  _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) {
-    return o.__proto__ || Object.getPrototypeOf(o);
-  };
-  return _getPrototypeOf(o);
-}
-
-(function () {
-  var JoomlaDropdownElement = /*#__PURE__*/function (_HTMLElement) {
-    _inherits(JoomlaDropdownElement, _HTMLElement);
-
-    var _super = _createSuper(JoomlaDropdownElement);
-
-    function JoomlaDropdownElement() {
-      _classCallCheck(this, JoomlaDropdownElement);
-
-      return _super.apply(this, arguments);
-    }
-
-    _createClass(JoomlaDropdownElement, [{
-      key: "for",
-      get: function get() {
-        return this.getAttribute('for');
-      },
-      set: function set(value) {
-        return this.setAttribute('for', value);
-      }
-    }, {
-      key: "connectedCallback",
-      value: function connectedCallback() {
-        var _this = this;
-
-        this.setAttribute('aria-labelledby', this["for"].substring(1));
-        var button = document.querySelector(this["for"]);
-        var innerLinks = this.querySelectorAll('a');
-
-        if (!button.id) {
-          return;
-        } // var children = [].slice.call( menu[getElementsByTagName]('*'));
-        // this.classList.add('dropdown');
+      if (!button.id) {
+        return;
+      } // var children = [].slice.call( menu[getElementsByTagName]('*'));
+      // this.classList.add('dropdown');
 
 
-        button.setAttribute('aria-haspopup', true);
-        button.setAttribute('aria-expanded', false);
-        button.addEventListener('click', function (event) {
-          if (_this.hasAttribute('expanded')) {
-            _this.removeAttribute('expanded');
+      button.setAttribute('aria-haspopup', true);
+      button.setAttribute('aria-expanded', false);
+      button.addEventListener('click', function (event) {
+        if (_this.hasAttribute('expanded')) {
+          _this.removeAttribute('expanded');
 
-            event.target.setAttribute('aria-expanded', false);
-          } else {
-            _this.setAttribute('expanded', '');
+          event.target.setAttribute('aria-expanded', false);
+        } else {
+          _this.setAttribute('expanded', '');
 
-            event.target.setAttribute('aria-expanded', true);
-          }
-
-          document.addEventListener('click', function (evt) {
-            if (evt.target !== button) {
-              if (!_this.findAncestor(evt.target, 'joomla-dropdown')) {
-                _this.close();
-              }
-            }
-          });
-          innerLinks.forEach(function (innerLink) {
-            innerLink.addEventListener('click', function () {
-              _this.close();
-            });
-          });
-        });
-      }
-      /*eslint-disable */
-
-      /* Method to dispatch events */
-
-    }, {
-      key: "dispatchCustomEvent",
-      value: function dispatchCustomEvent(eventName) {
-        var OriginalCustomEvent = new CustomEvent(eventName);
-        OriginalCustomEvent.relatedTarget = this;
-        this.dispatchEvent(OriginalCustomEvent);
-        this.removeEventListener(eventName, this);
-      }
-    }, {
-      key: "adoptedCallback",
-      value: function adoptedCallback(oldDocument, newDocument) {}
-    }, {
-      key: "attributeChangedCallback",
-      value: function attributeChangedCallback(attr, oldValue, newValue) {
-        switch (attr) {}
-      }
-      /* eslint-enable */
-
-    }, {
-      key: "close",
-      value: function close() {
-        var button = document.querySelector("#".concat(this.getAttribute('aria-labelledby')));
-        this.removeAttribute('expanded');
-        button.setAttribute('aria-expanded', false);
-      }
-      /* eslint-disable */
-
-    }, {
-      key: "findAncestor",
-      value: function findAncestor(el, tagName) {
-        while ((el = el.parentElement) && el.nodeName.toLowerCase() !== tagName) {
-          ;
+          event.target.setAttribute('aria-expanded', true);
         }
 
-        return el;
+        document.addEventListener('click', function (evt) {
+          if (evt.target !== button) {
+            if (!_this.findAncestor(evt.target, 'joomla-dropdown')) {
+              _this.close();
+            }
+          }
+        });
+        innerLinks.forEach(function (innerLink) {
+          innerLink.addEventListener('click', function () {
+            _this.close();
+          });
+        });
+      });
+    }
+    /*eslint-disable */
+
+    /* Method to dispatch events */
+
+  }, {
+    key: "dispatchCustomEvent",
+    value: function dispatchCustomEvent(eventName) {
+      var OriginalCustomEvent = new CustomEvent(eventName);
+      OriginalCustomEvent.relatedTarget = this;
+      this.dispatchEvent(OriginalCustomEvent);
+      this.removeEventListener(eventName, this);
+    }
+  }, {
+    key: "adoptedCallback",
+    value: function adoptedCallback(oldDocument, newDocument) {}
+  }, {
+    key: "attributeChangedCallback",
+    value: function attributeChangedCallback(attr, oldValue, newValue) {}
+    /* eslint-enable */
+
+  }, {
+    key: "close",
+    value: function close() {
+      var button = document.querySelector("#".concat(this.getAttribute('aria-labelledby')));
+      this.removeAttribute('expanded');
+      button.setAttribute('aria-expanded', false);
+    }
+    /* eslint-disable */
+
+  }, {
+    key: "findAncestor",
+    value: function findAncestor(el, tagName) {
+      while ((el = el.parentElement) && el.nodeName.toLowerCase() !== tagName) {
+        ;
       }
-      /* eslint-enable */
 
-    }], [{
-      key: "observedAttributes",
-      get:
-      /* Attributes to monitor */
-      function get() {
-        return ['for'];
-      }
-    }]);
+      return el;
+    }
+    /* eslint-enable */
 
-    return JoomlaDropdownElement;
-  }( /*#__PURE__*/_wrapNativeSuper(HTMLElement));
+  }], [{
+    key: "observedAttributes",
+    get:
+    /* Attributes to monitor */
+    function get() {
+      return ['for'];
+    }
+  }]);
 
-  customElements.define('joomla-dropdown', JoomlaDropdownElement);
-})();
+  return JoomlaDropdownElement;
+}( /*#__PURE__*/_wrapNativeSuper(HTMLElement));
 
-},{}]},{},[1]);
+customElements.define('joomla-dropdown', JoomlaDropdownElement);
---
+++ rollup.config.js
@@ -0,0 +1,93 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from "rollup-plugin-terser";
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import sass from 'rollup-plugin-sass';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+import postcss from 'postcss';
+import { existsSync } from 'fs';
+import { mkdir, writeFile } from 'fs/promises';
+import { dirname } from 'path';
+
+const Elements = [
+  'alert',
+  'collapse',
+  'dropdown',
+  'modal',
+  'panels',
+  'tab',
+  'tip',
+];
+
+const buildSettings = async () => {
+  const finalSettings = [];
+
+  Elements.forEach((file) => {
+    finalSettings.push({
+      input: `src/js/${file}/${file}.js`,
+      plugins: [nodeResolve()],
+      output: [
+        { file: `dist/js/joomla-${file}.js`, format: 'esm' },
+        { file: `docs/_media/js/joomla-${file}.js`, format: 'esm' },
+        { file: `dist/js/joomla-${file}.min.js`, format: 'esm', plugins: [terser()] },
+        { file: `docs/_media/js/joomla-${file}.min.js`, format: 'esm', plugins: [terser()] },
+      ],
+    });
+
+    finalSettings.push({
+      input: `src/js/${file}/${file}.js`,
+      plugins: [nodeResolve(), getBabelOutputPlugin({ presets: ['@babel/preset-env'] })],
+      output: [
+        { file: `dist/js/joomla-${file}-es5.js`, format: 'esm' },
+        { file: `docs/_media/js/joomla-${file}-es5.js`, format: 'esm' },
+        { file: `dist/js/joomla-${file}-es5.min.js`, format: 'esm', plugins: [getBabelOutputPlugin({ presets: ['@babel/preset-env'] }), terser()] },
+        { file: `docs/_media/js/joomla-${file}-es5.min.js`, format: 'esm', plugins: [getBabelOutputPlugin({ presets: ['@babel/preset-env'] }), terser()] },
+      ],
+    });
+  });
+
+  Elements.forEach((scssFile) => {
+    finalSettings.push({
+      input: `src/scss/${scssFile}/${scssFile}.scss`,
+      plugins: [
+        sass({
+          output: false,
+          processor: css => postcss([autoprefixer])
+            .process(css)
+            .then(async (result) => {
+              const path1 = `dist/css/joomla-${scssFile}.css`;
+              const path2 = `docs/_media/css/joomla-${scssFile}.css`;
+              if (!existsSync(dirname(path1))) {
+                await mkdir(dirname(path1), {recursive: true})
+              }
+              await writeFile(path1, result.css, {encoding: 'utf8'});
+              if (!existsSync(dirname(path2))) {
+                await mkdir(dirname(path2), {recursive: true})
+              }
+              await writeFile(path2, result.css, {encoding: 'utf8'});
+
+              postcss([autoprefixer, cssnano])
+              .process(result.css)
+              .then(async (result) => {
+                const path1 = `dist/css/joomla-${scssFile}.min.css`;
+                const path2 = `docs/_media/css/joomla-${scssFile}.min.css`;
+                if (!existsSync(dirname(path1))) {
+                  await mkdir(dirname(path1), {recursive: true})
+                }
+                await writeFile(path1, result.css, {encoding: 'utf8'});
+                if (!existsSync(dirname(path2))) {
+                  await mkdir(dirname(path2), {recursive: true})
+                }
+                await writeFile(path2, result.css, {encoding: 'utf8'});
+
+              });
+            })
+        }),
+      ],
+    });
+  })
+
+  return finalSettings;
+};
+
+export default buildSettings();
