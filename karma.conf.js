20071d904cc8cbb30288c0e6badb3251ea4a3318
dgrammatiko <d.grammatiko@gmail.com>
2021-07-17 15:15:16 +0200

    Ok, headless then

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,7 @@
 // Karma configuration
 module.exports = function (config) {
   config.set({
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
     // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine', 'fixture'],
 
     // list of files / patterns to load in the browser
@@ -50,19 +46,18 @@ module.exports = function (config) {
       // 'packags/src/**/js/*.js': ['coverage'],
       'tests/**/*.html': ['html2js'],
     },
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: process.cwd(),
     // web server port
     port: 9876,
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
     // Start these browsers, currently available:
     // - Chrome
     // - ChromeCanary
@@ -71,8 +66,7 @@ module.exports = function (config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
-
+    browsers: ['ChromeHeadless'],
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
